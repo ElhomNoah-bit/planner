d2f5fafd771ab07706b3a39b819a3a6b945013af
Noah Cannavo <elhomnoah@icloud.com>
2025-10-27 18:36:49 +0100

    Add autogenerated files for Noah Planner project
    - Created moc_predefs.h with preprocessor definitions for the project.
    - Added mocs_compilation.cpp to include generated moc files for various components.
    - Initialized timestamp file for tracking build changes.
    - Implemented qmltyperegistrations.cpp for QML type registration in the Noah Planner module.

---
+++ build/CMakeFiles/noah_planner.dir/build/.qt/rcc/qrc_noah_planner_raw_qml_0_extra_qmldirs.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/noah_planner.dir/build/.qt/rcc/qrc_noah_planner_raw_qml_0_extra_qmldirs.cpp.o: \
+ /home/noah/Dokumente/dev/project_study_planner/noah_planner_v2_cpp_qt6/build/.qt/rcc/qrc_noah_planner_raw_qml_0_extra_qmldirs.cpp \
+ /usr/include/stdc-predef.h
